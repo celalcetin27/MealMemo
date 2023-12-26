e09ed3b89304695d39ed248a9651a3629b6a10f2
HasanCanOrtatepe <hasancanortatepe58@gmail.com>
2023-12-27 01:49:13 +0300

    hco init

--- server/manage.py
+++ server/manage.py
@@ -2,8 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
-
+# celal amk
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
